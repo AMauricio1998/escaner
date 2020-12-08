00fd430bdb820472d28747cc0b11885843fca087
Jan Carlo Guzman Sanchez <al221910462@gmail.com>
2020-12-08 07:09:57 +0100

    Video 21

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,23 +1,22 @@
+import 'dart:io';
+
 import 'package:escaner/src/bloc/scans_bloc.dart';
 
-import 'package:escaner/src/pages/direcciones_page.dart'; 
+import 'package:escaner/src/pages/direcciones_page.dart';
 import 'package:escaner/src/pages/mapas_page.dart';
+import 'package:escaner/src/utils/utils.dart' as utils;
 import 'package:flutter/material.dart';
 
 import '../models/scan_model.dart';
 
-
-
 // import 'package:barcode_scan/barcode_scan.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   final scansBloc = new ScansBloc();
 
   int currentIndex = 0;
@@ -29,7 +28,7 @@ class _HomePageState extends State<HomePage> {
         title: Text('QR Scanner'),
         actions: <Widget>[
           IconButton(
-            icon : Icon( Icons.delete_forever ),
+            icon: Icon(Icons.delete_forever),
             onPressed: scansBloc.borrarScansTODOS,
           )
         ],
@@ -38,50 +37,59 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: _crearbottomNavigationBar(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child: Icon( Icons.filter_center_focus ),
+        child: Icon(Icons.filter_center_focus),
         onPressed: _scanQR,
         backgroundColor: Theme.of(context).primaryColor,
-        ),
+      ),
     );
   }
 
   _scanQR() async {
     // https://fernando-herrera.com
     // geo:40.724233047051705,-74.00731459101564
-      //String futureString = '';
-      String futureString = 'https:fernando-herrera.com';
- 
-      // try {
-      //   futureString = await BarcodeScanner.scan();
-      // } catch(e) {
-      //   futureString = e.toString();
-      // }
+    //String futureString = '';
+    String futureString = 'https:fernando-herrera.com';
 
-      if( futureString !=null){
-        final scan = ScanModel(valor: futureString);
-        scansBloc.agregarScan(scan);
+    // try {
+    //   futureString = await BarcodeScanner.scan();
+    // } catch(e) {
+    //   futureString = e.toString();
+    // }
+
+    if (futureString != null) {
+      final scan = ScanModel(valor: futureString);
+      scansBloc.agregarScan(scan);
+      final scan2 =
+          ScanModel(valor: 'geo:40.724233047051705,-74.00731459101564');
+      scansBloc.agregarScan(scan2);
+
+      if (Platform.isIOS) {
+        Future.delayed(Duration(milliseconds: 750), () {
+          utils.abrirScan(scan);
+        });
+      } else {
+        utils.abrirScan(scan);
       }
-      // print ('Future String: $futureString');
-      //  if ( futureString != null ){
-      //    print('tenemos informacion');
-      //  }
+    }
+    // print ('Future String: $futureString');
+    //  if ( futureString != null ){
+    //    print('tenemos informacion');
+    //  }
   }
 
-
-  Widget _callPage ( int paginaActual ) {
-
-    switch( paginaActual ) {
-
-      case 0: return MapasPage();
-      case 1: return DireccionesPage();
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
 
       default:
         return MapasPage();
     }
   }
 
-  Widget _crearbottomNavigationBar(){
-
+  Widget _crearbottomNavigationBar() {
     return BottomNavigationBar(
       currentIndex: currentIndex,
       onTap: (index) {
@@ -90,14 +98,9 @@ class _HomePageState extends State<HomePage> {
         });
       },
       items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapas')),
         BottomNavigationBarItem(
-          icon: Icon( Icons.map ),
-          title: Text('Mapas')
-          ),
-          BottomNavigationBarItem(
-          icon: Icon( Icons.brightness_5 ),
-          title: Text('Direcciones')
-          ),
+            icon: Icon(Icons.brightness_5), title: Text('Direcciones')),
       ],
     );
   }
